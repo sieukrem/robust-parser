1376fbf4bd1948a60f18a2aa0ae60c868eb5c0e5
Eugen Kremer <eugen.kremer@siemens.com>
2020-03-07 20:52:05 +0100

    Add implementation and tests

---
+++ src/parser.test.ts
@@ -0,0 +1,35 @@
+import * as assert from "chai";
+import { Parser } from "./Parser";
+
+describe("Parser",()=>{
+    describe("ctor",()=>{
+        it("throws on redeclaration of rule", ()=>{
+            assert.expect(()=>{
+                const parser = new Parser({ startRules:["rule1"], rules: [{name:"rule1", regex:/./g}, {name:"rule1",regex:/./g}]}); 
+            }).to.throw();
+        });
+        it("throws on non global regex definition", ()=>{
+            assert.expect(()=>{
+                const parser = new Parser({ startRules:["rule1"], rules:[{name:"rule1",regex:/./}]}); 
+            }).to.throw();
+        });
+        it("accepts proper definition", ()=>{
+                const parser = new Parser({ startRules:["rule1"], rules:[{name:"rule1",regex:/./g}]}); 
+        });
+    });
+
+    describe("parse",()=>{
+        it("calls fn when matches", ()=>{
+            let calledTimes : number = 0;
+            const testee = new Parser({ startRules : ["rule1"], rules:[
+                {name:"rule1", regex: /abc/g, fn: (p,match, isCalledAsStop )=>{
+                    calledTimes++;
+                    }
+                }]
+            });
+            testee.parse("abc   abc cba"),
+
+            assert.expect(calledTimes).to.eq(2);
+        });
+    });
+});
---
+++ src/parser.ts
@@ -0,0 +1,87 @@
+export interface IRule{
+    name : string;
+    regex : RegExp;
+    inner?: string[];
+    stops?: string[];
+    fn?: ((parser: Parser, match:RegExpExecArray, isCalledAsStop:boolean)=>void);
+}
+
+export interface IRules{
+    rules: IRule[];
+    startRules: string[];
+}
+
+export class Parser{
+    private getHashOfRules(rules:IRules):any{
+        const hash : any = {};
+        for(var i=0; i<rules.rules.length; i++){
+            var rule = rules.rules[i];
+            if (hash[rule.name]) {
+                throw new Error("State with the same name exists");
+            }
+            if (!rule.regex.global) {
+                throw new Error("Expect regex definition with 'g' flag");
+            }
+                
+            hash[rule.name] = rule;
+        }
+        return hash;
+    }
+
+    private getClosestMatch(rulesOfContext:string[], position:number, text:string): {match:RegExpExecArray, rule:IRule}|null{
+        let last : {match:RegExpExecArray, rule:IRule}|null = null;
+        for(var i=0; i<rulesOfContext.length;i++){
+            var rule = this.hashOfRules[rulesOfContext[i]];
+            rule.regex.lastIndex = position;
+            var match = rule.regex.exec(text);
+            if(!match) {
+                continue;
+            }
+            
+            if (!(!last || last.match.index > match.index)) {
+                continue;
+            }
+            
+            last = {match:match, rule:rule};
+        }
+        return last;
+    }
+
+    private hashOfRules:any;
+    private context:any;
+    private state:any;
+
+    public parse(text:string):void{
+        this.context = { prev:null, rules: this.rules.startRules || [], stops:[] };
+        this.state = {pos:0, text:text};
+        while(this.context){
+            // handle stop rules
+            
+            const closestMatch = this.getClosestMatch(this.context.stops.concat(this.context.rules), this.state.pos, text);
+            if (!closestMatch){ // nothing found leave current context
+                this.context = this.context.prev;
+                continue;
+            }
+            
+            const isStop = this.context.stops.indexOf(closestMatch.rule.name)>=0;
+            
+            if (closestMatch.rule.fn) {
+                closestMatch.rule.fn(this, closestMatch.match, isStop);
+            }
+
+            if (isStop){
+                this.context = this.context.prev;
+                continue;
+            }
+
+            this.state.pos = closestMatch.rule.regex.lastIndex;
+            
+            if (closestMatch.rule.inner) {
+                this.context = { prev: this.context, rules: closestMatch.rule.inner, stops: closestMatch.rule.stops||[] };
+            }
+        }
+    }
+    public constructor(private readonly rules:IRules){
+        this.hashOfRules = this.getHashOfRules(rules);
+	}
+}
